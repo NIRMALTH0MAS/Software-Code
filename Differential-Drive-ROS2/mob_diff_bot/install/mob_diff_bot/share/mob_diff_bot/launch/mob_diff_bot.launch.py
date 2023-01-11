8b839000caa5ac05f37ae955003e69c164cb059d
NIRMALTH0MAS <nirmal304@yahoo.com>
2023-01-11 10:40:53 +0530

    mob_diff_bot_Build Package Removed
    Build, Install & log package Removed

--- Differential-Drive-ROS2/mob_diff_bot/install/mob_diff_bot/share/mob_diff_bot/launch/mob_diff_bot.launch.py
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/Desktop/Differential-Drive-ROS2/mob_diff_bot/launch/mob_diff_bot.launch.py
--- Differential-Drive-ROS2/mob_diff_bot/install/mob_diff_bot/share/mob_diff_bot/launch/mod_diff_bot_sim.launch.py
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/Desktop/Differential-Drive-ROS2/mob_diff_bot/launch/mod_diff_bot_sim.launch.py
